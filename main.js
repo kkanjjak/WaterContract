969838d262d5589998eb82c00c7286da10008194
kkanjjak <112145268+kkanjjak@users.noreply.github.com>
2022-11-04 14:33:30 +0900

    ver.1.1

--- main.js
+++ main.js
@@ -9,3 +9,32 @@ function Indicator(e) {
     underLine.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight+ 2+ "px"
 
 }
+
+document.getElementById('AddNew').addEventListener("click",
+function(){
+var template = `<tr>
+<td class="numberTD">
+    <input type="text" class="number">
+</td>
+<td class="companyTD">
+    <input type="text" class="company">
+</td>
+
+<td class="titleTD">
+    <input type="text" class="title">
+</td>
+<td class="priceTD">
+    <input type="text" class="price">
+</td>
+<td class="dateTD">
+    <input type="text" class="date">
+</td>
+<td class="nameTD">
+    <input type="text" class="name">
+</td>
+<td class="departmentTD">
+    <input type="text" class="department">
+</td>
+</tr>`;
+document.querySelector('tbody').insertAdjacentHTML('beforeend', template);
+})
